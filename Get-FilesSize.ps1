dbea328ec74b81fef4773c1352c8328a04e687aa
user13 <user13@thinkwright.in>
2024-06-06 23:29:09 +0530

    adding second set of file to branch

---
+++ Get-FilesSize.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.Synopsis
+   Cheking Files in a Folder
+.DESCRIPTION
+   This will show the available files in folder
+.EXAMPLE
+   Get-FileSize -Folder Test1
+.EXAMPLE
+   More Example
+.INPUTS
+   Inputs to this cmdlet (if any)
+   UserName, Email Address, ID
+.OUTPUTS
+   Output from this cmdlet (if any)
+   User mailbox current size
+.NOTES
+   General notes
+.COMPONENT
+   The component this cmdlet belongs to
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+
+param($folderName, $drive)
+Write-host "Checking the Files in the Folder $Folder"
+Write-Host "Providing the Size of the Files in the $Folder"
---
+++ Get-MailboxSize.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.Synopsis
+   Mailbox Size Query
+.DESCRIPTION
+   This script can assist to get the mailbox size above 50 GB
+.EXAMPLE
+   Get-MailboxSize -UserName 
+.EXAMPLE
+   More Example
+.INPUTS
+   Inputs to this cmdlet (if any)
+   UserName, Email Address, ID
+.OUTPUTS
+   Output from this cmdlet (if any)
+   User mailbox current size
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+
+param($Username, $Username2)
+Write-Host "Checking the Size for the $Username"
+
+Write-host "Mailbox size is below for the $username"
+
