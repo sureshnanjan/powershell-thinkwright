df9c64aa5f7be4e5163f7d892128f4482a26d6a0
user14 <user14@thinkwright.in>
2024-06-06 16:28:36 +0530

    Adding first Script

---
+++ Grant-SMBXPermission.ps1
@@ -0,0 +1,7 @@
+﻿<##>
+
+param($username,$SMBXname,$Accessrights)
+Write-Host "User Name:" $username
+Write-Host "Shared Mailbox Name:" $SMBXname
+Write-Host "Level of Permission (Full Access, Send As or Send on Behalf):" $Accessrights
+Write-Host $Accessrights "Permission has been granted to" $username "on the mailbox" $SMBXname
---
+++ Remove-SMBXPermission.ps1
@@ -0,0 +1,5 @@
+﻿param($username,$SMBXname,$Accessrights)
+Write-Host "User Name:" $username
+Write-Host "Shared Mailbox Name:" $SMBXname
+Write-Host "Level of Permission (Full Access, Send As or Send on Behalf):" $Accessrights
+Write-Host $Accessrights "Permission has been removed for" $username "on the mailbox" $SMBXname
