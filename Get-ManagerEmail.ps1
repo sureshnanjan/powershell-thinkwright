fd7d6001872a1411a1e2d2fbb2eafa930351d047
user12 <user12@thinkwright.in>
2024-06-06 16:23:42 +0530

    Uploading Couple of New scripts developed

---
+++ Get-ManagerEmail.ps1
@@ -0,0 +1,31 @@
+﻿<#
+   .Synopsis
+      Returns Managers email address for username provided.
+   .DESCRIPTION
+      Returns Managers email address for username provided.
+   .EXAMPLE
+      Get-ManagerEmail -User <username>
+   .EXAMPLE
+      Another example of how to use this cmdlet
+   .INPUTS
+      Provide the username as input
+   .OUTPUTS
+      Manager Email
+   .NOTES
+      General notes
+   .COMPONENT
+      The component this cmdlet belongs to
+   .ROLE
+      The role this cmdlet belongs to
+   .FUNCTIONALITY
+      The functionality that best describes this cmdlet
+   #>
+
+   param(
+   [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+   [string[]]$User
+   )
+
+   Write-Host "Fetching Manager's email Address" -ForegroundColor Yellow
+   $MgrEmail = "$($user).mgr@email.com"
+   return $MgrEmail
---
+++ Report-Services.ps1
@@ -0,0 +1,38 @@
+﻿<#
+   .Synopsis
+      Generate a report of Windows Services and thier status.
+   .DESCRIPTION
+      Generates the html report of services with their status.
+   .EXAMPLE
+      To report only Running Services
+      Report-Services -Status Running
+   .EXAMPLE
+      To report only Stopped Services
+      Report-Services -Status Stopped
+   .INPUTS
+      
+   .OUTPUTS
+      
+   .NOTES
+      General notes
+   .COMPONENT
+      The component this cmdlet belongs to
+   .ROLE
+      The role this cmdlet belongs to
+   .FUNCTIONALITY
+      The functionality that best describes this cmdlet
+   #>
+
+   param(
+   [string]$Status,
+   [string]$OutputPath = (Get-Location)
+   )
+
+   $d = (Get-Date).Date.ToString('ddMMyyyy-hhmm')
+   if ($Status -eq "")
+   {$ServiceData = Get-Service | Select Name, DisplayName, ServiceType | Sort-Object Status | ConvertTo-Html }
+   else 
+   {$ServiceData = Get-Service | where {$_.Status -eq $Status} | Select Name, DisplayName, ServiceType | ConvertTo-Html}
+   
+   $ServiceData | Out-File "$OutputPath\ServiceStatus_$d.html"
+
