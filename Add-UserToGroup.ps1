5eca0ddc7cd8930c3458a0c55a9a5313aa689d95
user15 <user15@thinkwright.in>
2024-06-06 16:01:45 +0530

    Murugan-NewScript for Add users to Group

---
+++ Add-UserToGroup.ps1
@@ -0,0 +1,29 @@
+﻿<#
+.Synopsis
+   Add user to the group
+.DESCRIPTION
+   Adding AD user object to the AD group 
+.EXAMPLE
+   Add-UserToGroup.ps1 -username "user1" -groupname "ADGroup1"
+.EXAMPLE
+   Another example of how to use this cmdlet
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   The component this cmdlet belongs to
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+
+
+Param($Username,$GroupName)
+
+Write-Host "Adding $Username to the AD group: $GroupName"
+
+
